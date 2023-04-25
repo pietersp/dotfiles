9f01450d1f9f2f5611a8aa30927d81167f75fa17
Pieter Prinsloo <pietersp@gmail.com>
2023-04-25 23:44:29 +0200

    Add orgmode to test

--- stow/lvim/.config/lvim/config.lua
+++ stow/lvim/.config/lvim/config.lua
@@ -35,7 +35,12 @@ lvim.builtin.which_key.mappings["t"] = {
   r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
 }
 
--- -- Change theme settings
+lvim.builtin.which_key.mappings["o"] = {
+  name = "Org",
+  -- Populated by nvim-orgmode plugin
+}
+
+-- Change theme settings
 lvim.colorscheme = "onedark"
 
 lvim.builtin.alpha.active = true
@@ -166,6 +171,14 @@ lvim.plugins = {
     end
   },
   { "tpope/vim-surround" },
+  {
+    "nvim-orgmode/orgmode",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require('orgmode').setup_ts_grammar()
+      require('orgmode').setup {}
+    end
+  },
 }
 
 require("dap").configurations.scala = {
