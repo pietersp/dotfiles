8b42ef211ee1dda60ccf2bafafea4d78d563fc62
Pieter Prinsloo <gitlab-account@invicta.net.za>
2023-04-25 20:04:33 +0200

    Add wezterm initial config

---
+++ stow/wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font("FiraCode Nerd Font", {weight="Medium", stretch="Normal", style="Normal"}),
+  enable_tab_bar = false,
+  color_scheme = "OneDark (base16)"
+}
