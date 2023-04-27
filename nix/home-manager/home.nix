6b37fb0df799840bf80becd3b3288e8fd8fc9b14
Pieter Prinsloo <pietersp@gmail.com>
2023-04-28 01:27:38 +0200

    Clean up and remove zoxide
    zsh-autocomplete already has a way to switch to recent directories

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -1,22 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "pieter";
   home.homeDirectory = "/home/pieter";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     bottom
     cht-sh
@@ -57,16 +46,6 @@
     # '';
   };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/pieter/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
     EDITOR = "nvim";
   };
@@ -114,12 +93,6 @@
     };
   };
 
-  # zoxide (smart cd alternative)
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
   # zsh and plugins
   programs.zsh = {
     enable = true;
@@ -133,13 +106,11 @@
     ";
     shellAliases = {
       htop = "btm";
-      cd = "z";
     };
 
     zplug = {
       enable = true;
       plugins = [
-        # { name = "zsh-users/zsh-autosuggestions"; } 
         { name = "marlonrichert/zsh-autocomplete"; }
       ];
     };
