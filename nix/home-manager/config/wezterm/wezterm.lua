d50f886396440baaeec9c3dc4c8f955bf3c97392
Pieter Prinsloo <pietersp@gmail.com>
2023-06-05 00:22:25 +0200

    Disable bell in wezterm

--- nix/home-manager/config/wezterm/wezterm.lua
+++ nix/home-manager/config/wezterm/wezterm.lua
@@ -3,5 +3,6 @@ local wezterm = require 'wezterm'
 return {
   font = wezterm.font("FiraCode Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" }),
   enable_tab_bar = false,
-  color_scheme = "Catppuccin Mocha"
+  color_scheme = "Catppuccin Mocha",
+  audible_bell = "Disabled"
 }
