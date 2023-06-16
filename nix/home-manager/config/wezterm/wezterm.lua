0a0e129a863450daca1d33d603b53343acb450c8
Pieter Prinsloo <pietersp@gmail.com>
2023-06-16 19:36:20 +0200

    Update pkgs and change wezterm opacity

--- nix/home-manager/config/wezterm/wezterm.lua
+++ nix/home-manager/config/wezterm/wezterm.lua
@@ -4,5 +4,6 @@ return {
   font = wezterm.font("FiraCode Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" }),
   enable_tab_bar = false,
   color_scheme = "Catppuccin Mocha",
+  window_background_opacity = 0.7,
   audible_bell = "Disabled"
 }
