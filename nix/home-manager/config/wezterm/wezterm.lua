3ddb91551b62ffe93a59b75c1a8905223d028fca
Pieter Prinsloo <pietersp@gmail.com>
2023-11-05 23:23:45 +0200

    Add emoji to fallback fonts

--- nix/home-manager/config/wezterm/wezterm.lua
+++ nix/home-manager/config/wezterm/wezterm.lua
@@ -1,7 +1,10 @@
 local wezterm = require 'wezterm'
 
 return {
-  font = wezterm.font("FiraCode Nerd Font", { weight = "Medium", stretch = "Normal", style = "Normal" }),
+  font = wezterm.font_with_fallback {
+    { family = "FiraCode Nerd Font", weight = "Medium", stretch = "Normal", style = "Normal" },
+    "Noto Color Emoji"
+  },
   enable_tab_bar = false,
   color_scheme = "Catppuccin Mocha",
   window_background_opacity = 0.7,
