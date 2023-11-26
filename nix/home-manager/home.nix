1d73773d0da99c224aa121471d7c2bca2a36f25a
Pieter Prinsloo <pietersp@gmail.com>
2023-11-26 13:38:44 +0200

    Make git completions sane by not sorting and add prettyness

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -322,6 +322,25 @@ in
     # Help for commands
     zstyle ':fzf-tab:complete:-command-:*' fzf-preview '(out=$(tldr --color always "$word") 2>/dev/null && echo $out) || (out=$(MANWIDTH=$FZF_PREVIEW_COLUMNS man "$word") 2>/dev/null && echo $out) || (out=$(which "$word") && echo $out)' 
     zstyle ':fzf-tab:complete:*:*' fzf-preview 'less ''${(Q)realpath}'
+    # git commands
+    zstyle ':completion:*:git-(checkout|log|show):*' sort false
+    zstyle ':fzf-tab:complete:git-(add|diff|restore):*' fzf-preview \
+      'git diff $word | delta'
+    zstyle ':fzf-tab:complete:git-log:*' fzf-preview \
+      'git log --color=always $word'
+    zstyle ':fzf-tab:complete:git-help:*' fzf-preview \
+      'git help $word | bat -plman --color=always'
+    zstyle ':fzf-tab:complete:git-show:*' fzf-preview \
+      'case "$group" in
+      "commit tag") git show --color=always $word ;;
+      *) git show --color=always $word | delta ;;
+      esac'
+    zstyle ':fzf-tab:complete:git-checkout:*' fzf-preview \
+      case "$group" in
+      "[modified file]") git diff $word | delta ;;
+      "[recent commit object name]") git show --color=always $word | delta ;;
+      *) git log --color=always $word ;;
+      esac'
     '';
 
     sessionVariables = {
