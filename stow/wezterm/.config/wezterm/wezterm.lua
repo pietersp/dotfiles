b21ede3364cf153540b460f7cd16fc0edcd82114
Pieter Prinsloo <pietersp@gmail.com>
2023-12-28 13:40:54 +0200

    Remove outdated stow setup

--- stow/wezterm/.config/wezterm/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require 'wezterm'
-
-return {
-  font = wezterm.font("FiraCode Nerd Font", {weight="Medium", stretch="Normal", style="Normal"}),
-  enable_tab_bar = false,
-  color_scheme = "OneDark (base16)"
-}
