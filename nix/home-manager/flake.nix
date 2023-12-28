236e3b820aefd78675fa42f111b01790958070d3
Pieter Prinsloo <pietersp@gmail.com>
2023-12-28 13:14:29 +0200

    Add initial configuration.nix

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -1,39 +1,31 @@
 {
-  description = "Home Manager configuration of pieter";
+  description = "My NixOs and Home Manager configuration flake";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."pieter" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      nixosConfigurations = {
+        nixos-tutorial = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
       };
-      homeConfigurations."pieterprinsloo" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+      
+      homeConfigurations = {
+        pieter = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
       };
     };
 }
