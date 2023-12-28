e2e9544cd9900bfb75ca1b5fe617345c78f65e20
Pieter Prinsloo <pietersp@gmail.com>
2023-12-28 14:50:52 +0200

    Add hyprland cachix
    This allows installation of hyprland without building it yourself

--- nix/home-manager/configuration.nix
+++ nix/home-manager/configuration.nix
@@ -125,6 +125,9 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
  
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
- 
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 }
