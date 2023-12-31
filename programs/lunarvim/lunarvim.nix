8f0894227833fe9fd57465a85eeb43e6c33efef1
Pieter Prinsloo <pietersp@gmail.com>
2023-12-31 15:56:48 +0200

    Update lunarvim to latest in master

--- programs/lunarvim/lunarvim.nix
+++ programs/lunarvim/lunarvim.nix
@@ -1,15 +1,18 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 let
   lunarvim = pkgs.lunarvim.overrideAttrs (old: {
     src = pkgs.fetchFromGitHub {
       owner = "LunarVim";
       repo = "LunarVim";
-      rev = "f74046d1911e430ca734d1ed1509d2ff3bdfe7e1";
-      sha256 = "z1Cw3wGpFDmlrAIy7rrjlMtzcW7a6HWSjI+asEDcGPA=";
+      rev = "b124e8c3e3f8145029c0d9aeb3912e5ac314e0a2";
+      sha256 = "J2E4BQfyrZ9HvuDDCLFm6wj8S9QQ2lEo9PwsJkjcbuY=";
     };
+    patches = [];
   });
 in {
-  home.packages = [ pkgs.lunarvim ];
+  home.packages = [ 
+    lunarvim
+  ];
 
   xdg.configFile.lvim = {
     source = ./config;
