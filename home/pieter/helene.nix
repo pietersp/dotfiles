916dd72f952ba2e6f4f031eb584e54d3f19c4b40
Pieter Prinsloo <pietersp@gmail.com>
2024-03-17 01:32:35 +0200

    Add basic hyprland module

---
+++ home/pieter/helene.nix
@@ -0,0 +1,11 @@
+{ inputs, lib, pkgs, config, outputs, ...}: 
+{
+  imports = [
+    # Required configs
+    common/core #required
+
+    # Host specific optional configs
+    common/optional/wezterm
+    common/optional/desktops/hyprland
+  ];
+}
