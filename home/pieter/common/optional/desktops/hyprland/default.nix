8b1553c8156143486c7a2285867f677c493dcf22
Pieter Prinsloo <pietersp@gmail.com>
2024-03-21 19:31:56 +0200

    Hypr: Map escape to caps

--- home/pieter/common/optional/desktops/hyprland/default.nix
+++ home/pieter/common/optional/desktops/hyprland/default.nix
@@ -10,17 +10,20 @@
     enable = true;
     xwayland.enable = true;
     systemd.enable = true;
-  };
 
-  # settings = {
-  #   env = [
-  #     "NIXOS_OZONE_WL, 1" # for ozone-based and electron apps to run on wayland
-  #     # "MOZ_ENABLE_WAYLAND, 1" # for firefox to run on wayland
-  #     # "MOZ_WEBRENDER, 1" # for firefox to run on wayland
-  #     # "XDG_SESSION_TYPE,wayland"
-  #     # "WLR_NO_HARDWARE_CURSORS,1"
-  #     # "WLR_RENDERER_ALLOW_SOFTWARE,1"
-  #     # "QT_QPA_PLATFORM,wayland"
-  #   ];
-  # };
+    settings = {
+      env = [
+        "NIXOS_OZONE_WL, 1" # for ozone-based and electron apps to run on wayland
+        # "MOZ_ENABLE_WAYLAND, 1" # for firefox to run on wayland
+        # "MOZ_WEBRENDER, 1" # for firefox to run on wayland
+        # "XDG_SESSION_TYPE,wayland"
+        # "WLR_NO_HARDWARE_CURSORS,1"
+        # "WLR_RENDERER_ALLOW_SOFTWARE,1"
+        # "QT_QPA_PLATFORM,wayland
+      ];
+      input = {
+        kb_options = "escape:swap"
+      };
+    };
+  };
 }
