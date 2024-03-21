ecc442b5d033eee5fa0c852df4d54a69dbb0095f
Pieter Prinsloo <pietersp@gmail.com>
2024-03-21 17:39:00 +0200

    Remove F1-F12 workspaces for hyprland and disable env vars for testing

--- home/pieter/common/optional/desktops/hyprland/default.nix
+++ home/pieter/common/optional/desktops/hyprland/default.nix
@@ -11,12 +11,12 @@
 
   settings = {
     env = [
-      "NIXOS_OZONE_WL, 1" # for ozone-based and electron apps to run on wayland
-      "MOZ_ENABLE_WAYLAND, 1" # for firefox to run on wayland
-      "MOZ_WEBRENDER, 1" # for firefox to run on wayland
-      "XDG_SESSION_TYPE,wayland"
-      "WLR_NO_HARDWARE_CURSORS,1"
-      "WLR_RENDERER_ALLOW_SOFTWARE,1"
+      # "NIXOS_OZONE_WL, 1" # for ozone-based and electron apps to run on wayland
+      # "MOZ_ENABLE_WAYLAND, 1" # for firefox to run on wayland
+      # "MOZ_WEBRENDER, 1" # for firefox to run on wayland
+      # "XDG_SESSION_TYPE,wayland"
+      # "WLR_NO_HARDWARE_CURSORS,1"
+      # "WLR_RENDERER_ALLOW_SOFTWARE,1"
       # "QT_QPA_PLATFORM,wayland"
     ];
   };
