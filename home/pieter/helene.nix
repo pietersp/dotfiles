55a4995bb8dac2f401fa4c8448ebb95843581253
Pieter Prinsloo <pietersp@gmail.com>
2024-03-24 19:40:33 +0200

    Change scaling for hyprland on helene

--- home/pieter/helene.nix
+++ home/pieter/helene.nix
@@ -13,5 +13,8 @@
     # Host specific optional configs
     common/optional/wezterm
     common/optional/desktops/hyprland
+    common/optional/ides/intellij
   ];
+
+  wayland.windowManager.hyprland.settings.monitor = "eDP-1, 1920x1080, 0x0, 1.0";
 }
