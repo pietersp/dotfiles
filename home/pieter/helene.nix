80c431f79065eda2c165b47a1717727a60d1df37
Pieter Prinsloo <pietersp@gmail.com>
2024-04-06 11:55:21 +0200

    Add keepass and also catppuccin theme for gtk and qt

--- home/pieter/helene.nix
+++ home/pieter/helene.nix
@@ -15,6 +15,19 @@
     common/optional/desktops/hyprland
     common/optional/ides/intellij
   ];
+  home.packages = with pkgs; [
+    keepass
+  ];
+
+  gtk.enable = true;
+  gtk.theme.package = pkgs.catppuccin-gtk;
+  gtk.theme.name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+  qt.enable = true;
+  qt.style.package = pkgs.catppuccin-kvantum;
+  qt.style.name = "catppuccin-kvantum";
+  qt.platformTheme = "gtk";
+
+
 
   wayland.windowManager.hyprland.settings.monitor = "eDP-1, 1920x1080, 0x0, 1.0";
 }
