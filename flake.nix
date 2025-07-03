1a0f7948fb93269cf94d75bc27a18388fd5e6bf0
= <=>
2025-07-03 17:03:39 +0200

    Fully qualify nix pkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOs and Home Manager configuration flake";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
