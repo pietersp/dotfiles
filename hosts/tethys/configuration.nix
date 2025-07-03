de88c145f0ad94012d13642b0dae8ecef9d5593f
Pieter <pieter@tethys.local>
2025-07-03 16:53:12 +0200

    Fix initial darwin

--- hosts/tethys/configuration.nix
+++ hosts/tethys/configuration.nix
@@ -6,52 +6,32 @@
   pkgs,
   ...
 }: {
+  # Needed for determinate systems install
+  nix.enable = false;
+
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    coreutils
-    curl
-    gnumake
-    gnused
-    gnutar
-    unzip
     vim
-    wget
-    zsh
   ];
 
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "zap";
+  };
+
   # Use our custom packages
-  nixpkgs.overlays = [outputs.overlays.additions outputs.overlays.modifications outputs.overlays.unstable];
+  #nixpkgs.overlays = [outputs.overlays.additions outputs.overlays.modifications outputs.overlays.unstable];
 
-  # Set DNS servers.
-  networking.dns = ["1.1.1.1" "1.0.0.1"];
+  # Set Git commit hash for darwin-version.
+  # system.configurationRevision = self.rev or self.dirtyRev or null;
 
-  # Set the system shell.
-  users.defaultUserShell = pkgs.zsh;
-
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
-  # programs.fish.enable = true;  # Not recommended to use this along with fish.
-
-  # https://github.com/LnL7/nix-darwin/issues/29
-  # security.pam.enable = true;
+  system.primaryUser = "pieter";
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  system.stateVersion = 6;
 
   # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
-
-  # Link home-manager
-  home-manager = {
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    users.pieter = import ../../home/pieter/tethys.nix;
-    extraSpecialArgs = {inherit inputs outputs;};
-  };
 }
