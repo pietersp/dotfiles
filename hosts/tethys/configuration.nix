fdf2c6cda8e8d522c70bce8ad20e1bbd7e17c16f
= <=>
2025-07-03 20:01:40 +0200

    Add wezterm and firacode nerd font to tethys

--- hosts/tethys/configuration.nix
+++ hosts/tethys/configuration.nix
@@ -13,6 +13,7 @@
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     vim
+    wezterm
   ];
 
   homebrew = {
@@ -22,6 +23,10 @@
       "raycast"
     ];
   };
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+  ];
 
   # Use our custom packages
   #nixpkgs.overlays = [outputs.overlays.additions outputs.overlays.modifications outputs.overlays.unstable];
