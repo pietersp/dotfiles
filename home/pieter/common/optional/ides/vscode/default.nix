0959961d62d5859a317d61ec17266e70f26d892c
Pieter Prinsloo <pietersp@gmail.com>
2025-07-23 00:24:55 +0200

    Add vscode extensions for metals

--- home/pieter/common/optional/ides/vscode/default.nix
+++ home/pieter/common/optional/ides/vscode/default.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: {
   programs.vscode = {
     enable = true;
+    profiles.default.extensions = with pkgs.vscode-extensions; [
+      vscodevim.vim
+      scalameta.metals
+      scala-lang.scala
+    ];
   };
 }
