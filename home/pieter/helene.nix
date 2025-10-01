de4edb7e73516406cff256c01f0a56ebb206dcde
Pieter Prinsloo <pietersp@gmail.com>
2025-10-01 20:07:03 +0200

    Remove hyprland for helene (exp with omarchy)

--- home/pieter/helene.nix
+++ home/pieter/helene.nix
@@ -12,7 +12,7 @@
 
     # Host specific optional configs
     common/optional/wezterm
-    common/optional/desktops/hyprland
+    # common/optional/desktops/hyprland
     common/optional/ides/intellij
   ];
   home.packages = with pkgs; [
@@ -21,15 +21,15 @@
     pulsemixer
   ];
 
-  gtk.enable = true;
-  gtk.theme.package = pkgs.catppuccin-gtk;
-  gtk.theme.name = "Catppuccin-Macchiato-Compact-Pink-Dark";
-  qt.enable = true;
-  qt.style.package = pkgs.catppuccin-kvantum;
-  qt.style.name = "catppuccin-kvantum";
-  qt.platformTheme.name = "gtk";
+  # gtk.enable = true;
+  # gtk.theme.package = pkgs.catppuccin-gtk;
+  # gtk.theme.name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+  # qt.enable = true;
+  # qt.style.package = pkgs.catppuccin-kvantum;
+  # qt.style.name = "catppuccin-kvantum";
+  # qt.platformTheme.name = "gtk";
 
 
 
-  wayland.windowManager.hyprland.settings.monitor = "eDP-1, 1920x1080, 0x0, 1.0";
+  # wayland.windowManager.hyprland.settings.monitor = "eDP-1, 1920x1080, 0x0, 1.0";
 }
