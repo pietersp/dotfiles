5e984b664703fe756a996f0571b1db62281a25c1
Pieter Prinsloo <pietersp@gmail.com>
2026-05-03 17:19:29 +0200

    feat(starship): show active devenv profile

--- home/pieter/common/core/starship/default.nix
+++ home/pieter/common/core/starship/default.nix
@@ -7,6 +7,12 @@
     settings = {
       scala.disabled = true;
       container.disabled = true;
+      custom.devenv_profile = {
+        when = ''test -n "$DEVENV_ACTIVE_PROFILES"'';
+        command = ''printf "%s" "$DEVENV_ACTIVE_PROFILES"'';
+        format = ''[devenv:$output]($style) '';
+        style = "bold peach";
+      };
     };
   };
 }
