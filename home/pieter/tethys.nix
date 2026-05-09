44d7aa6f136386891d02cd3a41a4ccafb2a6bc76
Pieter Prinsloo <pietersp@gmail.com>
2026-05-09 23:20:30 +0200

    Add python3 to tethys

--- home/pieter/tethys.nix
+++ home/pieter/tethys.nix
@@ -5,7 +5,26 @@
   config,
   pkgs,
   ...
-}: {
+}: let
+  catppuccinObsidianTheme = pkgs.stdenvNoCC.mkDerivation {
+    pname = "catppuccin-obsidian";
+    version = "0.4.47";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "obsidian";
+      rev = "55aa9c9036a9df864593fdaedb2a227d3823fb06";
+      sha256 = "0lanq9wpkxsp8554lx5a11l2z7c7imy62hcwxggp344rfkd1l7dq";
+    };
+
+    installPhase = ''
+      runHook preInstall
+      install -Dm644 manifest.json "$out/manifest.json"
+      install -Dm644 theme.css "$out/theme.css"
+      runHook postInstall
+    '';
+  };
+in {
   imports = [
     ./common/core
     # ./common/optional/desktops # No desktop for mac
@@ -22,11 +41,24 @@
   home.packages = with pkgs; [
     # Avoid mpv-with-scripts here because yt-dlp now pulls in deno.
     mpv-unwrapped
+    python3
     zed-editor
     outputs.packages.${pkgs.stdenv.hostPlatform.system}.gemini-cli
     outputs.packages.${pkgs.stdenv.hostPlatform.system}.codex-cli
     outputs.packages.${pkgs.stdenv.hostPlatform.system}.check-cli-versions
   ];
+
+  programs.obsidian = {
+    enable = true;
+    cli.enable = true;
+    defaultSettings.themes = [catppuccinObsidianTheme];
+
+    vaults.Vault = {
+      target = "Documents/Vaults/Vault";
+    };
+  };
+
+  home.file."Documents/Vaults/Vault/.obsidian/appearance.json".force = true;
 
   programs.zsh.shellAliases = {
     docker = "podman";
